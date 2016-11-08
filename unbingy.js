35be0cb22cd6e69efa7e3f6ab12136d1248dbc16
Ryan Cooper <ryancooper729@gmail.com>
2016-11-07 23:21:26 -0500

    add unbingy.js code

---
+++ unbingy.js
@@ -0,0 +1,17 @@
+var bingHeader = document.getElementById("b_header")
+bingHeader.remove();
+
+var taskBar = document.getElementsByClassName("taskBar");
+taskBar[0].remove();
+
+var microsoftNav = document.getElementById("MicrosoftNav");
+microsoftNav.remove();
+
+var feedbackLinks = document.getElementsByClassName("fdbkLinks");
+feedbackLinks[0].remove();
+
+var bingFooter = document.getElementsByClassName("b_footer");
+bingFooter[0].remove();
+
+var bottomRight = document.getElementsByClassName("bottomRightOverlay");
+bottomRight[0].remove();
